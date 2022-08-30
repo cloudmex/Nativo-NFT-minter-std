f35f6b47e5c3ae47eb8c4be3b3ca1ed79dd6eb38
Joe Hank <jcanalesn9@gmail.com>
2022-08-30 14:53:47 -0600

    Upgrade migrate.rs
    Changes in the method  remote-upgrade

--- src/migrate.rs
+++ src/migrate.rs
@@ -12,7 +12,7 @@ impl Contract {
     #[cfg(target_arch = "wasm32")]
     pub fn upgrade(self) {
         use near_sys as sys;
-        // assert!(env::predecessor_account_id() == self.minter_account_id);
+        assert!(env::predecessor_account_id() == self.owner_id);
         //input is code:<Vec<u8> on REGISTER 0
         //log!("bytes.length {}", code.unwrap().len());
         const GAS_FOR_UPGRADE: u64 = 20 * TGAS; //gas occupied by this fn
